b55637679fd4c4cdad08910b7bba6562f6fb98ee
回笑看今朝 <nyqty@qq.com>
2021-09-24 17:45:58 +0800

    Update jd_bean_sign.js

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -8,6 +8,7 @@ JRBODY抓取网站:ms.jr.jd.com/gw/generic/hy/h5/m/appSign(进入金融APP签到
 建议云函数用户使用配置文件部署方式,直接在config分支新建JRBODY.txt即可
 其他环境用户除了JRBODY环境变量可以选用JRBODY.txt文件,放在同目录下,规则同上一行AC用户.
 注:优先识别环境变量,如使用txt文件请不要设置环境变量.JRBODY换行符(应为unix换行符)可能影响脚本读取!
+Env(京东多合一签到SCF开始)
  */
 console.log('京东多合一签到SCF开始')
 const sendNotify = require('./sendNotify.js').sendNotify
