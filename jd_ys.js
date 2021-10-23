267575697818a7935e819142c81c57a1641299c4
回笑看今朝 <nyqty@qq.com>
2021-10-23 21:49:57 +0800

    更新

--- jd_ys.js
+++ jd_ys.js
@@ -72,18 +72,13 @@ let allMessage = '';
       AuthorizationInfo[$.UserName] = $.Authorization
     }
   }
-  let res = await getAuthorShareCode('https://raw.githubusercontent.com/Aaron-lv/updateTeam/master/shareCodes/ys.json')
+  let res = await getAuthorShareCode('https://raw.githubusercontent.com/atyvcn/updateTeam/master/shareCodes/jd/ys.json')
   if (!res) {
-    $.http.get({url: 'https://purge.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/ys.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
+    $.http.get({url: 'https://purge.jsdelivr.net/gh/atyvcn/updateTeam@master/shareCodes/jd/ys.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
     await $.wait(1000)
-    res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/ys.json')
+    res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/atyvcn/updateTeam@master/shareCodes/jd/ys.json')
   }
-  let res2 = await getAuthorShareCode('https://raw.githubusercontent.com/zero205/updateTeam/main/shareCodes/ys.json')
-  if (!res2) {
-    await $.wait(1000)
-    res2 = await getAuthorShareCode('https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/ys.json')
-  }
-  $.shareCodes = [...new Set([...$.shareCodes, ...(res || []), ...(res2 || [])])]
+  $.shareCodes = [...new Set([...$.shareCodes, ...(res || [])])]
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
     $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
@@ -291,7 +286,8 @@ function active(shareId = null, type = true) {
                 } else {
                   console.log(`\n抽奖次数：${num}，开始抽奖`)
                 }
-                for (let i = 0; i < num; i++) {
+                $.stop = false
+                for (let i = 0; i < num && !$.stop; i++) {
                   await lottery()
                   await $.wait(2000)
                 }
@@ -345,6 +341,7 @@ function lottery() {
             if (data.code === 200) {
               if (data.data) {
                 console.log(`抽奖成功：获得${data.data.awardVal}${data.data.awardName}`)
+                num = 0
               } else {
                 console.log(`抽奖成功：获得空气~`)
                 num++
