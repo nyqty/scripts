f2a3a51214d4c6b5f3fb57c3f0d0e3cb84ef2ea7
回笑看今朝 <nyqty@qq.com>
2021-10-28 21:55:27 +0800

    Update jd_red.js

--- jd_red.js
+++ jd_red.js
@@ -1,6 +1,6 @@
 /*
 双十一无门槛红包🧧
-cron 0 12,18 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_red.js
+cron 0 0,12,18 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_red.js
 返利变量：FLCODE，默认给脚本作者返利，若需要返利给自己，请自己修改返利变量FLCODE；例：FLCODE="你的返利code"
 * */
 const $ = new Env('双十一无门槛红包🧧');
