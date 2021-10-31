bc8542b8bc57572d2f4473b817c175ed86689c7b
回笑看今朝 <nyqty@qq.com>
2021-10-31 17:08:32 +0800

    Revert "更新"
    This reverts commit 817be15faaf7dd2b4a54d5164ca59b48dcd53fce.

--- jd_jddj_fruit.js
+++ jd_jddj_fruit.js
@@ -16,7 +16,7 @@ cron "10 0,3,8,11,17 * * *" script-path=https://raw.githubusercontent.com/passer
 let isNotify = true;//是否通知,仅限nodejs
 let ckPath = './jdCookie.js';//ck路径,环境变量:JDDJ_CKPATH
 
-const $ = new API("京东到家果园");
+const $ = new API("jddj_fruit");
 try {
     //https://ghproxy.com/https://raw.githubusercontent.com/passerby-b/Script/master/jddj_fruit_code.js
     $.http.get({ url: 'https://raw.githubusercontents.com/passerby-b/Script/master/jddj_fruit_code.js' }).then(response => {
