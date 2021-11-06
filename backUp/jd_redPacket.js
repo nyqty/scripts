24ba705967107e47dfb1387854525607502bbdab
TY <nyqty@qq.com>
2021-11-06 13:50:44 +0800

    更新

--- backUp/jd_redPacket.js
+++ backUp/jd_redPacket.js
@@ -41,6 +41,7 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
+  /*
   let res = await getAuthorShareCode('https://raw.githubusercontent.com/atyvcn/updateTeam/master/shareCodes/jd/red.json')
   if (!res) {
     $.http.get({url: 'https://purge.jsdelivr.net/gh/atyvcn/updateTeam@master/shareCodes/jd/red.json'}).then((resp) => {}).catch((e) => $.log('刷新CDN异常', e));
@@ -48,6 +49,12 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/atyvcn/updateTeam@master/shareCodes/jd/red.json')
   }
   $.authorMyShareIds = [...(res && res || [])];
+  */
+  $.authorMyShareIds = []
+
+  console.log(arguments.callee.name.toString());
+  
+
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -288,7 +295,7 @@ async function red() {
 //获取任务列表API
 function taskHomePage() {
   return new Promise((resolve) => {
-    $.post(taskUrl(arguments.callee.name.toString(), {"clientInfo":{}}), (err, resp, data) => {
+    $.post(taskUrl(arguments.callee.name.toString(), {"clientInfo":{},"random":random(00000000, 99999999),"log":`${new Date()}~1aTmT9wMbemMDF3c0xyVzAxMQ==.`,"sceneid":"JLHBhPageh5"}), (err, resp, data) => {
       try {
         if (err) {
           console.log(`\n${$.name}: API查询请求失败 ‼️‼️`);
@@ -307,8 +314,9 @@ function taskHomePage() {
 //领取任务API,需token
 function startTask(taskType) {
   // 从taskHomePage返回的数据里面拿taskType
-  let data = {taskType};
+  let data = {taskType,"random":random(00000000, 99999999),"log":`${new Date()}~1aTmT9wMbemMDF3c0xyVzAxMQ==.`,"sceneid":"JLHBhPageh5"};
   data['token'] = $.md5($.md5("j" + JSON.stringify(data) + "D"))
+  //console.log(data);
   return new Promise((resolve) => {
     $.post(taskUrl(arguments.callee.name.toString(), data), (err, resp, data) => {
       try {
@@ -343,7 +351,7 @@ async function active(taskType) {
     } else {
       console.log(`任务列表为空,手动进入app内检查 是否存在[从京豆首页进领券中心逛30秒]的任务,如存在,请手动完成再运行脚本`)
       $.msg(`${$.name}`, '', '手动进入app内检查\n是否存在[从京豆首页进领券中心逛30秒]的任务\n如存在,请手动完成再运行脚本');
-      if ($.isNode()) await notify.sendNotify(`${$.name} - 账号${$.index} - ${$.nickName}`, `执行脚本出现异常\n请手动进入app内检查\n是否存在[从京豆首页进领券中心逛30秒]的任务\n如存在,请手动完成再运行脚本`)
+      //if ($.isNode()) await notify.sendNotify(`${$.name} - 账号${$.index} - ${$.nickName}`, `执行脚本出现异常\n请手动进入app内检查\n是否存在[从京豆首页进领券中心逛30秒]的任务\n如存在,请手动完成再运行脚本`)
     }
   } else {
     console.log(`---具体任务详情---${JSON.stringify(getTaskDetailForColorRes)}`);
@@ -352,7 +360,7 @@ async function active(taskType) {
 
 //获取具体任务详情API
 function getTaskDetailForColor(taskType) {
-  const data = {"clientInfo":{}, taskType};
+  const data = {"clientInfo":{}, taskType,"random":random(00000000, 99999999),"log":`${new Date()}~1aTmT9wMbemMDF3c0xyVzAxMQ==.`,"sceneid":"JLHBhPageh5"};
   return new Promise((resolve) => {
     $.post(taskUrl(arguments.callee.name.toString(), data), (err, resp, data) => {
       try {
@@ -373,7 +381,8 @@ function getTaskDetailForColor(taskType) {
 }
 //做成任务API
 function taskReportForColor(taskType, detailId) {
-  const data = {taskType, detailId};
+  const data = {taskType, detailId,"random":random(00000000, 99999999),"log":`${new Date()}~1aTmT9wMbemMDF3c0xyVzAxMQ==.`,"sceneid":"JLHBhPageh5"};
+
   data['token'] = $.md5($.md5("j" + JSON.stringify(data) + "D"))
   //console.log(`活动id：：：${detailId}\n`)
   return new Promise((resolve) => {
@@ -396,7 +405,7 @@ function taskReportForColor(taskType, detailId) {
 }
 //领取做完任务后的红包
 function receiveTaskRedpacket(taskType) {
-  const body = {"clientInfo":{}, taskType};
+  const body = {"clientInfo":{}, taskType,"random":random(00000000, 99999999),"log":`${new Date()}~1aTmT9wMbemMDF3c0xyVzAxMQ==.`,"sceneid":"JLHBhPageh5"};
   return new Promise((resolve) => {
     $.post(taskUrl('h5receiveRedpacketAll', body), (err, resp, data) => {
       try {
@@ -421,7 +430,7 @@ function receiveTaskRedpacket(taskType) {
 //助力API
 function jinli_h5assist(redPacketId) {
   //一个人一天只能助力两次，助力码redPacketId 每天都变
-  const body = {"clientInfo":{},redPacketId,"followShop":0,"promUserState":""};
+  const body = {"clientInfo":{},redPacketId,"followShop":0,"promUserState":"","random":random(00000000, 99999999),"log":`${new Date()}~1aTmT9wMbemMDF3c0xyVzAxMQ==.`,"sceneid":"JLHBhPageh5"};
   const options = taskUrl(arguments.callee.name.toString(), body)
   return new Promise((resolve) => {
     $.post(options, (err, resp, data) => {
@@ -451,7 +460,7 @@ function jinli_h5assist(redPacketId) {
 }
 //领取红包API
 function h5receiveRedpacketAll() {
-  const options = taskUrl(arguments.callee.name.toString(), {"clientInfo":{}})
+  const options = taskUrl(arguments.callee.name.toString(), {"clientInfo":{},"random":random(00000000, 99999999),"log":`${new Date()}~1aTmT9wMbemMDF3c0xyVzAxMQ==.`,"sceneid":"JLHBhPageh5"})
   return new Promise((resolve) => {
     $.post(options, (err, resp, data) => {
       try {
@@ -476,7 +485,7 @@ function h5receiveRedpacketAll() {
 }
 //发起助力红包API
 function h5launch() {
-  const body = {"clientInfo":{},"followShop":0,"promUserState":""};
+  const body = {"followShop":1,"random":random(000000, 999999),"log":"4817e3a2~8,~1wsv3ig","sceneid":"JLHBhPageh5"};
   const options = taskUrl(arguments.callee.name.toString(), body)
   return new Promise((resolve) => {
     $.post(options, (err, resp, data) => {
@@ -506,7 +515,7 @@ function h5launch() {
   })
 }
 function h5activityIndex() {
-  const body = {"clientInfo":{},"isjdapp":1};
+  const body = {"clientInfo":{},"isjdapp":1,"random":random(00000000, 99999999),"log":`${new Date()}~1aTmT9wMbemMDF3c0xyVzAxMQ==.`,"sceneid":"JLHBhPageh5"};
   const options = taskUrl(arguments.callee.name.toString(), body);
   return new Promise((resolve) => {
     $.post(options, async (err, resp, data) => {
@@ -687,7 +696,7 @@ function TotalBean() {
               $.nickName = data.data.userInfo.baseInfo.nickname;
             }
           } else {
-            console.log('京东服务器返回空数据');
+            console.log('GetJDUserInfoUnion 京东服务器返回空数据');
           }
         }
       } catch (e) {
@@ -710,6 +719,12 @@ function jsonParse(str) {
     }
   }
 }
+
+function random(min, max) {
+  let num = Math.floor(Math.random() * (max - min)) + min;
+  return `${num}`;
+}
+
 // md5
 !function(n){function t(n,t){var r=(65535&n)+(65535&t);return(n>>16)+(t>>16)+(r>>16)<<16|65535&r}function r(n,t){return n<<t|n>>>32-t}function e(n,e,o,u,c,f){return t(r(t(t(e,n),t(u,f)),c),o)}function o(n,t,r,o,u,c,f){return e(t&r|~t&o,n,t,u,c,f)}function u(n,t,r,o,u,c,f){return e(t&o|r&~o,n,t,u,c,f)}function c(n,t,r,o,u,c,f){return e(t^r^o,n,t,u,c,f)}function f(n,t,r,o,u,c,f){return e(r^(t|~o),n,t,u,c,f)}function i(n,r){n[r>>5]|=128<<r%32,n[14+(r+64>>>9<<4)]=r;var e,i,a,d,h,l=1732584193,g=-271733879,v=-1732584194,m=271733878;for(e=0;e<n.length;e+=16){i=l,a=g,d=v,h=m,g=f(g=f(g=f(g=f(g=c(g=c(g=c(g=c(g=u(g=u(g=u(g=u(g=o(g=o(g=o(g=o(g,v=o(v,m=o(m,l=o(l,g,v,m,n[e],7,-680876936),g,v,n[e+1],12,-389564586),l,g,n[e+2],17,606105819),m,l,n[e+3],22,-1044525330),v=o(v,m=o(m,l=o(l,g,v,m,n[e+4],7,-176418897),g,v,n[e+5],12,1200080426),l,g,n[e+6],17,-1473231341),m,l,n[e+7],22,-45705983),v=o(v,m=o(m,l=o(l,g,v,m,n[e+8],7,1770035416),g,v,n[e+9],12,-1958414417),l,g,n[e+10],17,-42063),m,l,n[e+11],22,-1990404162),v=o(v,m=o(m,l=o(l,g,v,m,n[e+12],7,1804603682),g,v,n[e+13],12,-40341101),l,g,n[e+14],17,-1502002290),m,l,n[e+15],22,1236535329),v=u(v,m=u(m,l=u(l,g,v,m,n[e+1],5,-165796510),g,v,n[e+6],9,-1069501632),l,g,n[e+11],14,643717713),m,l,n[e],20,-373897302),v=u(v,m=u(m,l=u(l,g,v,m,n[e+5],5,-701558691),g,v,n[e+10],9,38016083),l,g,n[e+15],14,-660478335),m,l,n[e+4],20,-405537848),v=u(v,m=u(m,l=u(l,g,v,m,n[e+9],5,568446438),g,v,n[e+14],9,-1019803690),l,g,n[e+3],14,-187363961),m,l,n[e+8],20,1163531501),v=u(v,m=u(m,l=u(l,g,v,m,n[e+13],5,-1444681467),g,v,n[e+2],9,-51403784),l,g,n[e+7],14,1735328473),m,l,n[e+12],20,-1926607734),v=c(v,m=c(m,l=c(l,g,v,m,n[e+5],4,-378558),g,v,n[e+8],11,-2022574463),l,g,n[e+11],16,1839030562),m,l,n[e+14],23,-35309556),v=c(v,m=c(m,l=c(l,g,v,m,n[e+1],4,-1530992060),g,v,n[e+4],11,1272893353),l,g,n[e+7],16,-155497632),m,l,n[e+10],23,-1094730640),v=c(v,m=c(m,l=c(l,g,v,m,n[e+13],4,681279174),g,v,n[e],11,-358537222),l,g,n[e+3],16,-722521979),m,l,n[e+6],23,76029189),v=c(v,m=c(m,l=c(l,g,v,m,n[e+9],4,-640364487),g,v,n[e+12],11,-421815835),l,g,n[e+15],16,530742520),m,l,n[e+2],23,-995338651),v=f(v,m=f(m,l=f(l,g,v,m,n[e],6,-198630844),g,v,n[e+7],10,1126891415),l,g,n[e+14],15,-1416354905),m,l,n[e+5],21,-57434055),v=f(v,m=f(m,l=f(l,g,v,m,n[e+12],6,1700485571),g,v,n[e+3],10,-1894986606),l,g,n[e+10],15,-1051523),m,l,n[e+1],21,-2054922799),v=f(v,m=f(m,l=f(l,g,v,m,n[e+8],6,1873313359),g,v,n[e+15],10,-30611744),l,g,n[e+6],15,-1560198380),m,l,n[e+13],21,1309151649),v=f(v,m=f(m,l=f(l,g,v,m,n[e+4],6,-145523070),g,v,n[e+11],10,-1120210379),l,g,n[e+2],15,718787259),m,l,n[e+9],21,-343485551),l=t(l,i),g=t(g,a),v=t(v,d),m=t(m,h)}return[l,g,v,m]}function a(n){var t,r="",e=32*n.length;for(t=0;t<e;t+=8){r+=String.fromCharCode(n[t>>5]>>>t%32&255)}return r}function d(n){var t,r=[];for(r[(n.length>>2)-1]=void 0,t=0;t<r.length;t+=1){r[t]=0}var e=8*n.length;for(t=0;t<e;t+=8){r[t>>5]|=(255&n.charCodeAt(t/8))<<t%32}return r}function h(n){return a(i(d(n),8*n.length))}function l(n,t){var r,e,o=d(n),u=[],c=[];for(u[15]=c[15]=void 0,o.length>16&&(o=i(o,8*n.length)),r=0;r<16;r+=1){u[r]=909522486^o[r],c[r]=1549556828^o[r]}return e=i(u.concat(d(t)),512+8*t.length),a(i(c.concat(e),640))}function g(n){var t,r,e="";for(r=0;r<n.length;r+=1){t=n.charCodeAt(r),e+="0123456789abcdef".charAt(t>>>4&15)+"0123456789abcdef".charAt(15&t)}return e}function v(n){return unescape(encodeURIComponent(n))}function m(n){return h(v(n))}function p(n){return g(m(n))}function s(n,t){return l(v(n),v(t))}function C(n,t){return g(s(n,t))}function A(n,t,r){return t?r?s(t,n):C(t,n):r?m(n):p(n)}$.md5=A}(this);
 // prettier-ignore
