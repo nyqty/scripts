06860be7c7f83a751738116206cfe7ac72102198
TY <nyqty@qq.com>
2022-01-01 12:53:47 +0800

    Update jd_opencardL37.js

--- jd_opencardL37.js
+++ jd_opencardL37.js
@@ -50,7 +50,7 @@ let activityCookie =''
     return;
   }
   $.activityId = "dzlhkklblzms20211228"
-  $.shareUuid = "609ba159b730437795603216e13ffd36"
+  $.shareUuid = "ea00f3a409a44afb8e52df1b2dd115d9"
   console.log(`入口:\nhttps://lzdz1-isv.isvjcloud.com/dingzhi/customized/common/activity?activityId=${$.activityId}&shareUuid=${$.shareUuid}`)
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
@@ -219,12 +219,6 @@ async function run() {
     if($.outFlag){
       console.log('此ip已被限制，请过10分钟后再执行脚本\n')
       return
-    }
-    console.log($.actorUuid)
-    console.log(`当前助力:${$.shareUuid}`)
-    if($.index == 1){
-      $.shareUuid = $.actorUuid
-      console.log(`后面的号都会助力:${$.shareUuid}`)
     }
     await $.wait(parseInt(Math.random() * 1000 + 5000, 10))
     if(flag) await $.wait(parseInt(Math.random() * 1000 + 10000, 10))
