a39ffc333e7152d3f9b5ab6387eec1fc12f01fad
TY <nyqty@qq.com>
2022-01-10 20:49:39 +0800

    Update jd_zhanianshou_help.js

--- jd_zhanianshou_help.js
+++ jd_zhanianshou_help.js
@@ -31,9 +31,8 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
         return;
     }
-    
-    $.shareCodesArr = [];
 
+    $.shareCodesArr = [];
     for (let i = 0; i < cookiesArr.length; i++) {
         if (cookiesArr[i]) {
             await getUA()
@@ -53,7 +52,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
         }
     }
 
-    for (let i = 60; i < cookiesArr.length; i++) {
+    for (let i = 0; i < cookiesArr.length; i++) {
         if (cookiesArr[i]) {
             await getUA()
             cookie = cookiesArr[i];
