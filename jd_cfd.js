4138d7aa1c3bb3577bb9c587d815dcb1096c45a5
TY <nyqty@qq.com>
2022-02-07 12:51:49 +0800

    更新

--- jd_cfd.js
+++ jd_cfd.js
@@ -40,7 +40,7 @@ let cookiesArr = [], cookie = '', token = '';
 let UA, UAInfo = {};
 let nowTimes;
 const randomCount = $.isNode() ? 20 : 3;
-$.appId = 10032;
+$.appId = "92a36";
 function oc(fn, defaultVal) {//optioanl chaining
   try {
     return fn()
@@ -94,31 +94,6 @@ if ($.isNode()) {
       await $.wait(3000);
     }
   }
-//  let res = await getAuthorShareCode('https://gitee.com/KingRan521/JD-Scripts/raw/master/shareCodes/cfd.json')
-//  $.strMyShareIds = [...(res || [])]
-//  await shareCodesFormat()
-//  for (let i = 0; i < cookiesArr.length; i++) {
-//    cookie = cookiesArr[i];
-//    $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
-//    $.canHelp = true
-//    UA = UAInfo[$.UserName]
-//    if ($.newShareCodes && $.newShareCodes.length) {
-//      console.log(`\n开始互助\n`);
-//      for (let j = 0; j < $.newShareCodes.length && $.canHelp; j++) {
-//        console.log(`账号${$.UserName} 去助力 ${$.newShareCodes[j]}`)
-//        $.delcode = false
-//        await helpByStage($.newShareCodes[j])
-//        await $.wait(3000)
-//        if ($.delcode) {
-//          $.newShareCodes.splice(j, 1)
-//          j--
-//          continue
-//        }
-//      }
-//    } else {
-//      break
-//    }
-//  }
   await showMsg();
 })()
     .catch((e) => $.logErr(e))
@@ -1687,7 +1662,7 @@ async function requestAlgo() {
       'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7'
     },
     'body': JSON.stringify({
-      "version": "1.0",
+      "version": "3.0",
       "fp": $.fingerprint,
       "appId": $.appId.toString(),
       "timestamp": Date.now(),
@@ -1750,7 +1725,7 @@ function decrypt(time, stk, type, url) {
     const hash2 = $.CryptoJS.HmacSHA256(st, hash1.toString()).toString($.CryptoJS.enc.Hex);
     // console.log(`\nst:${st}`)
     // console.log(`h5st:${["".concat(timestamp.toString()), "".concat(fingerprint.toString()), "".concat($.appId.toString()), "".concat(token), "".concat(hash2)].join(";")}\n`)
-    return encodeURIComponent(["".concat(timestamp.toString()), "".concat($.fingerprint.toString()), "".concat($.appId.toString()), "".concat($.token), "".concat(hash2)].join(";"))
+    return encodeURIComponent(["".concat(timestamp.toString()), "".concat($.fingerprint.toString()), "".concat($.appId.toString()), "".concat($.token), "".concat(hash2), "".concat("3.0"), "".concat(time)].join(";"))
   } else {
     return '20210318144213808;8277529360925161;10001;tk01w952a1b73a8nU0luMGtBanZTHCgj0KFVwDa4n5pJ95T/5bxO/m54p4MtgVEwKNev1u/BUjrpWAUMZPW0Kz2RWP8v;86054c036fe3bf0991bd9a9da1a8d44dd130c6508602215e50bb1e385326779d'
   }
