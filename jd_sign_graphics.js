931a2f1abb8884ad71c3cd31d1bd48dee110078b
TY <nyqty@qq.com>
2022-05-29 12:49:15 +0800

    更新

--- jd_sign_graphics.js
+++ jd_sign_graphics.js
@@ -6,7 +6,7 @@ npm i png-js 或者 npm i png-js -S
 
 */
 
-const Faker = require('./sign_graphics_validate.js')
+const Faker = require('./utils/sign_graphics_validate.js')
 const $ = new Env('京东签到翻牌');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
