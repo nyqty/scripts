8632906ad0fd94b002bc441c22cb3428adb7250b
TY <nyqty@qq.com>
2022-11-08 15:09:25 +0800

    Update jd_wish.js

--- jd_wish.js
+++ jd_wish.js
@@ -71,6 +71,7 @@ if ($.isNode()) {
         appName = appNameArr[j]
         console.log(`\n开始第${j + 1}个活动：${appName}\n`)
         await jd_wish();
+				await $.wait(2000)
       }
     }
   }
@@ -169,6 +170,7 @@ async function healthyDay_getHomeData(type = true) {
                   if (vo.taskType === 13 || vo.taskType === 12) {
                     console.log(`点击热区`)
                     await harmony_collectScore({ "appId": appId, "taskToken": vo.simpleRecordInfoVo.taskToken, "taskId": vo.taskId, "actionType": "0" }, vo.taskType)
+										await $.wait(1000)
                   } else {
                   console.log(`【${vo.taskName}】已完成\n`)
                 }
@@ -180,12 +182,14 @@ async function healthyDay_getHomeData(type = true) {
                   if (vo.taskType === 13 || vo.taskType === 12) {
                     console.log(`签到`)
                     await harmony_collectScore({ "appId": appId, "taskToken": vo.simpleRecordInfoVo.taskToken, "taskId": vo.taskId, "actionType": "0" }, vo.taskType)
+										await $.wait(1000)
                   } else if (vo.taskType === 1) {
                     for (let key of Object.keys(vo.followShopVo)) {
                       let followShopVo = vo.followShopVo[key]
                       if (followShopVo.status !== 2) {
                         console.log(`【${followShopVo.shopName}】${vo.subTitleName}`)
                         await harmony_collectScore({ "appId": appId, "taskToken": followShopVo.taskToken, "taskId": vo.taskId, "actionType": "0" })
+												await $.wait(1000)
                       }
                     }
                   } else if (vo.taskType === 5) {
@@ -194,6 +198,7 @@ async function healthyDay_getHomeData(type = true) {
                       if (browseShopVo.status !== 2) {
                         console.log(`【${browseShopVo.skuName}】${vo.subTitleName}`)
                         await harmony_collectScore({ "appId": appId, "taskToken": browseShopVo.taskToken, "taskId": vo.taskId, "actionType": "0" })
+												await $.wait(1000)
                       }
                     }
                   } else if (vo.taskType === 15) {
@@ -202,6 +207,7 @@ async function healthyDay_getHomeData(type = true) {
                       if (productInfoVos.status !== 2) {
                         console.log(`【${productInfoVos.skuName}】${vo.subTitleName}`)
                         await harmony_collectScore({ "appId": appId, "taskToken": productInfoVos.taskToken, "taskId": vo.taskId, "actionType": "0" })
+												await $.wait(1000)
                       }
                     }
                   } else if (vo.taskType === 3) {
@@ -210,6 +216,7 @@ async function healthyDay_getHomeData(type = true) {
                       if (shoppingActivityVos.status !== 2) {
                         console.log(`【${vo.subTitleName}】`)
                         await harmony_collectScore({ "appId": appId, "taskToken": shoppingActivityVos.taskToken, "taskId": vo.taskId, "actionType": "0" })
+												await $.wait(1000)
                       }
                     }
                   } else if (vo.taskType === 8) {
@@ -220,6 +227,7 @@ async function healthyDay_getHomeData(type = true) {
                         await harmony_collectScore({ "appId": appId, "taskToken": productInfoVos.taskToken, "taskId": vo.taskId, "actionType": "1" })
                         await $.wait(vo.waitDuration * 1000)
                         await harmony_collectScore({ "appId": appId, "taskToken": productInfoVos.taskToken, "taskId": vo.taskId, "actionType": "0" })
+												await $.wait(1000)
                       }
                     }
                   } else if (vo.taskType === 27 && vo.taskId === 18) {
@@ -235,6 +243,7 @@ async function healthyDay_getHomeData(type = true) {
                           await $.wait(vo.waitDuration * 1000)
                         }
                         await harmony_collectScore({ "appId": appId, "taskToken": shoppingActivityVos.taskToken, "taskId": vo.taskId, "actionType": "0" })
+												await $.wait(1000)
                       }
                     }
                   } else if (vo.taskType === 14) {
