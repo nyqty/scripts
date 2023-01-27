0d0511ccd7ca4c55e5b47bbde24066afc821baac
TY <nyqty@qq.com>
2023-01-27 14:00:21 +0800

    Update jd_ppdt1.js

--- jd_ppdt1.js
+++ jd_ppdt1.js
@@ -39,6 +39,7 @@ if ($.isNode()) {
                 continue
             }
             await xxx1();
+			if ($.ban) {console.log('风控，跳出');continue};
             await $.wait(500);
             await xxx2();
 			await $.wait(500);
@@ -46,7 +47,9 @@ if ($.isNode()) {
             await $.wait(500);
             await xxx3();
             await $.wait(500);
-            await xxx5();			
+            await xxx5();
+            await $.wait(500);
+            await xxx6();			
             await $.wait(2000);
 			
 			
@@ -81,7 +84,7 @@ async function xxx1() {
                     data = JSON.parse(data)
                     if (data.code == 0) {
                         if (data.data.bizCode == 0) {
-                            if (data.data?.result?.rewards) {
+                            if (data.data?.result?.rewards.length != 0) {
                                 if (data.data?.result?.rewards[0].awardType === 3) {
                                     console.log(` 恭喜获得 ${data.data?.result?.rewards[0].beanNum} 京豆`);
                                 } else {
@@ -90,6 +93,9 @@ async function xxx1() {
                             } else {
                                 console.log(JSON.stringify(data.data?.result));
                             }
+                        } else if (data.data.bizMsg.indexOf('风控')>-1) {
+                            console.log(data.data.bizMsg);
+							$.ban=true;
                         } else {
                             console.log(data.data.bizMsg);
                         }
@@ -124,7 +130,7 @@ async function xxx2() {
                     data = JSON.parse(data)
                     if (data.code == 0) {
                         if (data.data.bizCode == 0) {
-                            if (data.data?.result?.rewards) {
+                            if (data.data?.result?.rewards.length != 0) {
                                 if (data.data?.result?.rewards[0].awardType === 3) {
                                     console.log(` 恭喜获得 ${data.data?.result?.rewards[0].beanNum} 京豆`);
                                 } else {
@@ -168,7 +174,7 @@ async function xxx4() {
                     data = JSON.parse(data)
                     if (data.code == 0) {
                         if (data.data.bizCode == 0) {
-                            if (data.data?.result?.rewards) {
+                            if (data.data?.result?.rewards.length != 0) {
                                 if (data.data?.result?.rewards[0].awardType === 3) {
                                     console.log(` 恭喜获得 ${data.data?.result?.rewards[0].beanNum} 京豆`);
                                 } else {
@@ -212,7 +218,7 @@ async function xxx3() {
                     data = JSON.parse(data)
                     if (data.code == 0) {
                         if (data.data.bizCode == 0) {
-                            if (data.data?.result?.rewards) {
+                            if (data.data?.result?.rewards.length != 0) {
                                 if (data.data?.result?.rewards[0].awardType === 3) {
                                     console.log(` 恭喜获得 ${data.data?.result?.rewards[0].beanNum} 京豆`);
                                 } else {
@@ -240,6 +246,49 @@ async function xxx3() {
 async function xxx5() {
     let opt = {
         url: `https://api.m.jd.com/?client=wh5&appid=ProductZ4Brand&functionId=superBrandDoTask&t=1674700703306&body=%7B%22source%22:%22hall_1111%22,%22activityId%22:1012472,%22completionFlag%22:1,%22encryptProjectId%22:%222pNsbQpP8Jq7byJAMiXcQdWN7gSu%22,%22encryptAssignmentId%22:%22479JiY1PCk3xVrLGA6fvt3VAxgY3%22,%22assignmentType%22:0,%22actionType%22:0%7D`,
+        headers: {
+            'Origin': 'https://prodev.m.jd.com',
+            'User-Agent': $.UA,
+            'Cookie': cookie
+        }
+    }
+    return new Promise(async (resolve) => {
+        $.post(opt, async (err, resp, data) => {
+            try {
+                if (err) {
+                    console.log(`${JSON.stringify(err)}`)
+                    console.log(` API请求失败，请检查网路重试`)
+                } else {
+                    data = JSON.parse(data)
+                    if (data.code == 0) {
+                        if (data.data.bizCode == 0) {
+                            if (data.data?.result?.rewards.length != 0) {
+                                if (data.data?.result?.rewards[0].awardType === 3) {
+                                    console.log(` 恭喜获得 ${data.data?.result?.rewards[0].beanNum} 京豆`);
+                                } else {
+                                    console.log(JSON.stringify(data.data?.result?.rewards));
+                                }
+                            } else {
+                                console.log(JSON.stringify(data.data?.result));
+                            }
+                        } else {
+                            console.log(data.data.bizMsg);
+                        }
+                    } else {
+                        console.log(data.msg)
+                    }
+                }
+            } catch (e) {
+                $.logErr(e, resp)
+            } finally {
+                resolve(data)
+            }
+        })
+    })
+}
+async function xxx6() {
+    let opt = {
+        url: `https://api.m.jd.com/?client=wh5&appid=ProductZ4Brand&functionId=superBrandDoTask&t=1674780027902&body=%7B%22source%22:%22hall_1111%22,%22activityId%22:1012475,%22completionFlag%22:1,%22encryptProjectId%22:%2247yMVdNbddRfnKm5SKuUPfuid3LT%22,%22encryptAssignmentId%22:%223nrQJYLHKg3pHFWvd2oadunRbMWx%22,%22assignmentType%22:0,%22actionType%22:0%7D`,
         headers: {
             'Origin': 'https://prodev.m.jd.com',
             'User-Agent': $.UA,
