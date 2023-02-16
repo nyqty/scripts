fdbf51a1735b0948c2c46852e9c6af062d52fc3c
TY <nyqty@qq.com>
2023-02-16 17:20:14 +0800

    更新 jd_joyrunred.js

--- jd_joyrunred.js
+++ jd_joyrunred.js
@@ -27,7 +27,7 @@ if ($.isNode()) {
         return;
     }
 
-	for(let i=0;i<cookiesArr[__Oxf544a[29]];i++){
+	for(let i=0;i<cookiesArr.length;i++){
 		if(cookiesArr[i]){
 			cookie = cookiesArr[i];
 			$.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
