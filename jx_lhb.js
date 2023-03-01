667f41ab4a5204d024cee498abf471bc3315b018
TY <nyqty@qq.com>
2023-03-01 12:56:37 +0800

    更新 jx_lhb.js
    更新UA

--- jx_lhb.js
+++ jx_lhb.js
@@ -48,6 +48,7 @@ let _0x829415 = [];
     console.log("运行流程：助力--任务--抽奖");
     let authorCodeList = "103_121_104_119_97_84_72_110_48_77_85_53_83_105_102_71_77_118_65_76_55_65_61_61@SZk7ilpW8IMpg&53_84_55_83_54_79_71_49_57_75_77_72_47_113_65_80_65_83_113_81_75_65_61_61@S5KkcKmtOty-rfUG14oJM&70_43_75_120_52_66_54_56_72_98_77_76_107_110_53_98_116_47_51_71_55_81_61_61@S77k6Bk1H&66_78_43_43_121_118_80_51_51_100_50_51_71_43_71_107_110_98_100_90_74_103_61_61@S5KkcAkhNoweSeGGl87Bb&100_102_116_74_57_89_100_114_67_122_53_57_107_49_112_77_110_43_104_89_82_98_74_110_101_67_66_74_118_54_117_75_68_80_74_74_108_101_106_118_68_114_103_61@S5KkcRBtM8VfSIR_2lvEMIQ&110_47_48_115_71_84_116_104_112_116_98_103_101_119_70_97_97_84_81_49_84_103_61_61@S5KkcIGxutw-Dan6-0IlA&69_115_102_120_108_77_107_67_114_89_82_116_114_50_110_83_73_107_90_114_73_76_74_110_101_67_66_74_118_54_117_75_68_80_74_74_108_101_106_118_68_114_103_61@S5KkcRxce9lXQcx6lxvRedg".split("&");
     let authorCode = authorCodeList[Math.floor(Math.random() * authorCodeList.length)];
+    $.UAS={};
     if (JX_LHB_CODE) {
         JX_LHB_CODE = [...new Set([...JX_LHB_CODE, ...authorCodeList])];
         console.log("多余助力会助力TY");
@@ -59,7 +60,7 @@ let _0x829415 = [];
                 $.index = i + 1;
                 $.isLogin = true;
                 $.nickName = "";
-                $.UA = require("./USER_AGENTS").UARAM();
+                get_UA();
                 $.nohelp = false;
                 await islogin();
                 console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
@@ -103,15 +104,15 @@ let _0x829415 = [];
         console.log("未设置助力码,开始任务和抽奖");
     }
     console.log("\n开始任务和抽奖......");
-    for (let _0x49bd4a = 0; _0x49bd4a < cookiesArr.length; _0x49bd4a++) {
-        if (cookiesArr[_0x49bd4a]) {
-            cookie = cookiesArr[_0x49bd4a];
+    for (let i = 0; i < cookiesArr.length; i++) {
+        if (cookiesArr[i]) {
+            cookie = cookiesArr[i];
             $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
-            $.index = _0x49bd4a + 1;
+            $.index = i + 1;
             $.isLogin = true;
             $.nickName = "";
             $.flag = 1;
-            $.UA = require("./USER_AGENTS").UARAM();
+            get_UA();
             console.log("\n******开始【京东账号" + $.index + "】" + ($.nickName || $.UserName) + "*********\n");
             if (!$.isLogin) {
                 const _0x53a42d = {
@@ -148,33 +149,141 @@ let _0x829415 = [];
     $.done();
 });
 
+
+function randomString(e) {
+	e = e || 32;
+	let t = "0123456789abcdef",
+	a = t.length,
+	n = "";
+	for (let i = 0; i < e; i++)
+		n += t.charAt(Math.floor(Math.random() * a));
+	return n
+}
+
+
+function get_UA() {
+    if(!$.UAS[$.UserName]){
+        $.UAS[$.UserName]=createUA("jx","android");
+    }
+    $.UA=$.UAS[$.UserName];
+}
+
+function mt_rand(min, max) {
+    return Math.min(Math.floor(min + Math.random() * (max - min)), max);
+}
+
+
+function createUA(app="jd",os="android") {
+    if(app=="jd"){
+        app="jdapp";
+    }else{
+        app="jdpingou";
+        var session=mt_rand(51,99);//53
+    }
+    //22081212C Build/TKQ1.220829.002
+    if( os=="android" ){
+        const maketrans={
+            'K':'A','L':'B','M':'C','N':'D','O':'E','P':'F','Q':'G','R':'H','S':'I','T':'J','A':'K','B':'L','C':'M','D':'N','E':'O','F':'P','G':'Q','H':'R','I':'S','J':'T',
+            'o':'e','p':'f','q':'g','r':'h','s':'i','t':'j','u':'k','v':'l','w':'m','x':'n','e':'o','f':'p','g':'q','h':'r','i':'s','j':'t','k':'u','l':'v','m':'w','n':'x'
+        };
+        var dvs = ["MI9 Build/QKQ1.190825.002", "MI8 Build/OPM1.171019.026", "HLK-AL00 Build/HONORHLK-AL00", "SM-G9750 Build/QP1A.190711.020", "LIO-AL00 Build/HUAWEILIO-AL00", "ELE-AL00 Build/HUAWEIELE-AL00", "ANE-AL00 Build/HUAWEIANE-AL00", "22021211RC Build/SKQ1.211006.001"],
+        ivs = ["9", "10", "11", "12", "13"];
+        $.dv = dvs[Math.floor(Math.random() * dvs.length)];
+        $.iv = ivs[Math.floor(Math.random() * ivs.length)];
+        var ad = Buffer.from(randomString(16)).toString("base64"),
+        od = Buffer.from(randomString(16)).toString("base64"),
+        sv = Buffer.from($.iv).toString("base64").split("").map(val => maketrans[val] || val).join(""),
+        ov = Buffer.from("31").toString("base64").split("").map(val => maketrans[val] || val).join("");
+        od = od.split("").map(val => maketrans[val] || val).join("");
+        ad = ad.split("").map(val => maketrans[val] || val).join("");
+
+        if(app=="jdapp"){
+            //,"appname":""}
+            var avs = ["11.6.3","11.4.2"],//APP版本对应appBuild
+            bvs = ["98691","98638"];
+        }else{
+            var avs = ["5.50.2"];
+            bvs = ["22435"];
+        }
+        var i=Math.floor(Math.random() * avs.length);
+        $.av = avs[i];
+        $.bv = bvs[i];
+
+        var ep={
+            "hdid": "JM9F1ywUPwflvMIpYPok0tt5k9kW4ArJEU3lfLhxBqw=",
+            "ts": Date.now(),
+            "ridx": -1,
+            "cipher": {
+                "sv": sv,
+                "ad": ad,
+                "od": od,//...
+                "ov":ov,
+                "ud": ad
+            },
+            "ciphertype": 5,
+            "version": "1.2.0",
+            "appname": "com.jingdong.app.mall"
+        };
+        if(app=="jdapp"){
+            ;
+        }else{
+            ep.cipher={"bd":"","ad": ad,"sv": sv,"od": od,"ud": ad},
+            ep.appname="com.jd.pingou";
+        }
+        $.ep = encodeURIComponent(JSON.stringify(ep));
+    }else{
+        os="iPhone";
+    }
+    let ua=app+";"+os+";";
+    if(app=="jdapp"){
+        if(os=="android"){
+            ua+= $.av + `;;;appBuild/${$.bv};ef/1;ep/${$.ep};jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android {$.iv}; ${$.dv}; wv)`;
+        }else{
+            $.ADID = getUUID('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 1);
+            $.UUID = getUUID('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+            ua+=`9.5.4;13.6;${$.UUID};network/wifi;ADID/${$.ADID};model/iPhone10,3;addressid/0;appBuild/167668;jdSupportDarkMode/0;Mozilla/5.0 (iPhone; CPU iPhone OS 13_6 like Mac OS X)`;
+        }
+    }else if(app="jdpingou"){
+        if(os=="android"){
+            ua+=`${$.av};appBuild/${$.bv};session/${session};pap/JA2019_3111789;ef/1;ep/${ep};Mozilla/5.0 (Linux; Android ${$.iv}; ${$.dv}; wv)`;
+        }else{
+            $.ADID = getUUID('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 1);
+            $.UUID = getUUID('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+            ua+=`4.13.0;14.4.2;${UUID};network/wifi;model/iPhone10,2;appBuild/100609;ADID/${$.ADID};supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X)`
+        }
+    }
+
+    return ua+' AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36';
+}
+
+
 async function festivalhb_home(_0x3c372c = true) {
     return new Promise(async _0x386228 => {
-        $.get(await JDtaskUrl("festivalhb_home"), async (_0x1c93e3, _0x15a6a4, _0x1f3ae9) => {
+        $.get(await JDtaskUrl("festivalhb_home"), async (_0x1c93e3, _0x15a6a4, data) => {
             try {
                 if (_0x1c93e3) {
                     console.log("" + JSON.stringify(_0x1c93e3));
                     console.log(" API请求失败，请检查网路重试");
                 } else {
-                    _0x1f3ae9 = JSON.parse(_0x1f3ae9);
-                    if (_0x1f3ae9.code == 0) {
-                        _0x1f3ae9 = _0x1f3ae9.data;
-                        $.shareid = _0x1f3ae9.shareId;
+                    data = JSON.parse(data);
+                    if (data.code == 0) {
+                        data = data.data;
+                        $.shareid = data.shareId;
                         _0x829415.push($.shareid);
-                        $.lottery = _0x1f3ae9.drawChanceNum;
-                        $.tasklist = _0x1f3ae9.browseTaskList;
-                        $.helpid = _0x1f3ae9.helpTask.itemId;
-                        $.cash = _0x1f3ae9.totalCoinAmount;
-                        $.red = _0x1f3ae9.totalHbAmount;
+                        $.lottery = data.drawChanceNum;
+                        $.tasklist = data.browseTaskList;
+                        $.helpid = data.helpTask.itemId;
+                        $.cash = data.totalCoinAmount;
+                        $.red = data.totalHbAmount;
                         _0x3c372c && console.log("助力码:\n" + $.shareid + "@" + $.helpid);
                     } else {
-                        console.log(_0x1f3ae9.msg);
+                        console.log(data.msg);
                     }
                 }
             } catch (_0x1608ba) {
                 $.logErr(_0x1608ba, _0x15a6a4);
             } finally {
-                _0x386228(_0x1f3ae9);
+                _0x386228(data);
             }
         });
     });
